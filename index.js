130153d7d883c103627754ab18ca1d62859d62ad
lilac-electron <finpearson@gmail.com>
2024-06-23 19:34:04 +0100

    First app test

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ console.log(process.platform)
 console.log(process.env.USER)
 
 const { EventEmitter } = require('events');
+const { response } = require('express');
 const eventEmitter = new EventEmitter();
 eventEmitter.on('lunch', () => {
     console.log("Lunch time!!");
 });
 eventEmitter.emit('lunch');
 
+/*
 const {readFile, readFileSync} = require('fs');
 
 // This blocks any other processes and all code must execute before anything else may run.
@@ -28,4 +30,25 @@ console.log('test to see which is first')
 readFile('./testFile.txt', 'utf-8', (err, txt2) => {
     console.log(txt2)
 });
-console.log('test to see which is first')
+console.log('test to see which is first')*/
+
+// Using promises to make a non-blocking clean solution
+const {readFile} = require('fs').promises;
+async function testFunction() {
+    const file = await readFile('./testFile.txt', 'utf-8');
+};
+
+const myModule = require('./my-module')
+console.log(myModule);
+
+const app = express();
+app.get('/', (request, response) =>{
+    readFile('./index.html', 'utf-8', (err, html) =>{
+        if (err){
+            response.status(500).send('Sorry, not working!');
+        }
+        response.send(html);
+    })
+});
+
+app.listen(process.env.PORT || 3000)
---
+++ my-module.js
@@ -0,0 +1,3 @@
+module.exports = {
+    maths: 'juice'
+}
